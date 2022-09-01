c2520911b7d07393dd3485ef53822428e74b43f6
wesley <wesleynworisa@gmail.com>
2022-08-31 18:27:34 -0700

    cleaner attempt to make it work

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -11,12 +11,27 @@ export class CartComponent implements OnInit {
   constructor() { }
 
   cartitems:Product[] = gen_pholders(4)
+  usercart:object[]=[]
   ngOnInit(): void {
+    for(let i = 0; i> this.cartitems.length;i++)
+    {
+      // this.usercart.push({:})
+    }
   }
 
   remove(id:number)
   {
     delete this.cartitems[id]
+  }
+
+  totalprice()
+  {
+    let sum = 0
+    for(let i = 0; i < this.cartitems.length;i++)
+    {
+      sum += this.cartitems[i].price
+    }
+    return sum.toFixed(2)
   }
 
 }
